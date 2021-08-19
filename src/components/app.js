16c91bbc5c5e5e84669c05e2f742ffaa48e71547
Duel <marteiwgc@gmail.com>
2021-08-19 08:16:23 -0600

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h1>Weather</h1>
+        <h2>Code Challenge</h2>
+      </div>
+    );
+  }
+}
